2b0fe08505196b9e77e07e9af9b34d53a0713537
muhfadtz <mhfadtz@gmail.com>
2025-11-01 20:47:11 +0700

    updated

--- services/firebase.ts
+++ services/firebase.ts
@@ -27,15 +27,13 @@ import {
 import type { UserProfile, Space, ProgressReport } from "../types";
 
 // === Konfigurasi Firebase ===
-const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
-
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAU21BcQ1LMElQ8v0Pu4yQpBG-4WU8BK5c",
+  authDomain: "noya-f435a.firebaseapp.com",
+  projectId: "noya-f435a",
+  storageBucket: "noya-f435a.appspot.com",
+  messagingSenderId: "972657307551",
+  appId: "1:972657307551:web:0ec361184161f363f697aa",
 };
 
 // === Inisialisasi App ===
